0516f92a1fcd055d6dc133f7b128de94116b38af
Maarioo25 <maarioo2525@gmail.com>
2024-12-23 04:39:41 +0100

    Vistas creadas y funcionales, Todo el apartado visual terminado, Diccionario de idiomas implementado, falta por implementar el cambio de estilos y de idiomas desde la vista config y las animacions en los iconos.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using CatálogoDeProductos.Views;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,6 +8,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,10 +19,18 @@ namespace CatálogoDeProductos
     {
         public MainWindow()
         {
+            Idioma("en");
             InitializeComponent();
+            BordeInicio.Effect = null;
+            MainContent.Content = new InicioView();
         }
 
-        private void Header_MouseDown(object sender, MouseButtonEventArgs e)
+        private static void Idioma(String idioma)
+        {
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(idioma);
+        }
+
+        private void Arrastrar_Cabecera(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
             {
@@ -48,5 +59,48 @@ namespace CatálogoDeProductos
                 this.WindowState = WindowState.Normal;
             }
         }
+
+        private void btnInicio_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new InicioView();
+            resetearBordes();
+            BordeInicio.Effect = null;
+        }
+
+        private void btnProductos_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new ProductosView();
+            resetearBordes();
+            BordeProductos.Effect = null;
+        }
+
+        private void btnCategorias_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new CategoriasView();
+            resetearBordes();
+            BordeCategorias.Effect = null;
+        }
+
+        private void btnConfiguracion_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new ConfiguracionView();
+            resetearBordes();
+            BordeConfiguracion.Effect = null;
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void resetearBordes()
+        {
+            BordeInicio.Effect = new DropShadowEffect { Color = Colors.Black, BlurRadius = 10, ShadowDepth = 5 };
+            BordeProductos.Effect = new DropShadowEffect { Color = Colors.Black, BlurRadius = 10, ShadowDepth = 5 };
+            BordeCategorias.Effect = new DropShadowEffect { Color = Colors.Black, BlurRadius = 10, ShadowDepth = 5 };
+            BordeConfiguracion.Effect = new DropShadowEffect { Color = Colors.Black, BlurRadius = 10, ShadowDepth = 5 };
+            BordeSalir.Effect = new DropShadowEffect { Color = Colors.Black, BlurRadius = 10, ShadowDepth = 5 };
+        }
+
     }
 }
